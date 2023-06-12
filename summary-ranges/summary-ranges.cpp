aeda5dc842c5e177e3e9870b2e40ad880c56c756
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2023-06-12 18:27:48 +0530

    Time: 4 ms (34.63%) | Memory: 6.9 MB (13.50%) - LeetSync

--- summary-ranges/summary-ranges.cpp
+++ summary-ranges/summary-ranges.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string>ans;
+
         int n=nums.size();
         if(n==0)return ans;
+
         for(int i=0;i<nums.size();i++)
         {
             string temp;
+            //We just insert the element as it is 
             temp+=to_string(nums[i]);
+            
+            //We are checking for range ,whether this element will make range forward or not 
             if(i+1<nums.size() && nums[i+1]==nums[i]+1)
             {
                 while(i+1<nums.size() && nums[i+1]==nums[i]+1)
                 i++;
+
+                //Here we found that there is a range,so we insert a range in our ans
                 temp+="->";
                 temp+=to_string(nums[i]);
             }
