4a58f3f0db772d5a667e1d0e7d99112c4897c4ae
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2023-06-13 07:23:37 +0530

    Time: 259 ms (37.80%) | Memory: 40.2 MB (29.08%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -4,23 +4,28 @@ public:
     int equalPairs(vector<vector<int>>& grid) {
         vector<vector<int>>temp=grid;
         int n=grid.size();
+
+        //Here we transpose a matrix
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             if(i>j)
             swap(temp[i][j],temp[j][i]);
         }
+
+        //here we take map to store vector in form of string in map to reduce time complexity
         unordered_map<string,int>mp;
         int ans=0;
         for(int i=0;i<n;i++)
         {
             string temp="";
             for(int a:grid[i])
+            //here we store element+1 to avoid testcases like [[11,1],[1,11]]
             temp+=to_string(a+1);
-            cout<<temp<<" ";
             mp[temp]++;
         }
-        cout<<endl;
+
+        //checking for same row
         for(int i=0;i<n;i++)
         {
             string temp1="";
