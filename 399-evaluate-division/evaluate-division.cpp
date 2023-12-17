62b0f474e56e30a69a37dc39a66b27b4dc1de0b3
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2023-12-17 11:58:09 +0530

    Time: 5 ms (24.32%) | Memory: 9.3 MB (26.71%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,57 +1,50 @@
 class Solution {
-
+    
     double solve(string s,string d,unordered_map<string,vector<pair<string,double>>>&adj){
-        
-       
         queue<pair<double,string>>q;
-        map<string,int>mp;
         q.push({1,s});
+        map<string,int>mp;
         mp[s]++;
         while(!q.empty()){
             auto it=q.front();
             q.pop();
-            double temp=it.first;
-            
             string node=it.second;
-            cout<<node<<" ";
+            double temp=it.first;
             if(node==d)return temp;
-            
             for(auto i:adj[node]){
-                double nt=i.second;
+                double ntemp=i.second;
                 string nnode=i.first;
                 if(!mp.count(nnode)){
-                    q.push({nt*temp,nnode});
                     mp[nnode]++;
+                    q.push({ntemp*temp,nnode});
                 }
             }
-            
         }
         return -1;
     }
 
+
 public:
-    vector<double> calcEquation(vector<vector<string>>& e, vector<double>& values, vector<vector<string>>& q) {
+    vector<double> calcEquation(vector<vector<string>>& eq, vector<double>& values, vector<vector<string>>& queries) {
+
         vector<double>ans;
-
-        int n=e.size();
-
         unordered_map<string,vector<pair<string,double>>>adj;
-
+        int n=eq.size();
         for(int i=0;i<n;i++){
-            string n1=e[i][0],n2=e[i][1];
-            adj[n1].push_back({n2,values[i]});
-            adj[n2].push_back({n1,1/values[i]});
+            string s=eq[i][0],d=eq[i][1];
+            adj[s].push_back({d,values[i]});
+            adj[d].push_back({s,1/values[i]});
         }
 
-        for(int i=0;i<q.size();i++){
-            string s=q[i][0],d=q[i][1];
+        for(auto it:queries){
+            string s=it[0],d=it[1];
             if(!adj.count(s) || !adj.count(d))ans.push_back(-1);
             else{
-            double temp=solve(s,d,adj);
-            ans.push_back(temp);}
+                double temp=solve(s,d,adj);
+                ans.push_back(temp);
+            }
         }
 
         return ans;
-
     }
 };
