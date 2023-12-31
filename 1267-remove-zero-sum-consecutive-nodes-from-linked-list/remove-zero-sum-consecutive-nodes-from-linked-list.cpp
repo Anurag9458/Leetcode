a5b4783cab17cc92d06dd6e1c4038b5adb4d958c
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2023-12-31 14:39:27 +0530

    Time: 98 ms (5.01%) | Memory: 11.5 MB (69.36%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+    void solve(ListNode *head,int sum){
+    int temp=0;
+    ListNode *curr=head,*prev;
+
+    while(curr && temp!=sum){
+        temp+=curr->val;
+        cout<<temp<<" ";
+        prev=curr;
+        curr=curr->next;
+    }
+    cout<<endl;
+    // cout<<prev->val<<" ";
+    
+    temp=0;
+
+    while(curr){
+        temp+=curr->val;
+        curr=curr->next;
+        if(temp==0){
+            break;
+        }
+    }
+    // if(curr)
+    // cout<<curr->val<<" ";
+    if(temp==0)
+    prev->next=curr;
+    return;
+}
+
+
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        
+    map<int,int>mp;
+    int sum=0;
+    ListNode *curr=head;
+    while(curr){
+        sum+=curr->val;
+        // cout<<sum<<" ";
+        if(sum==0){
+            head=curr->next;
+            mp.clear();
+        }
+        else if(mp.count(sum)){
+            solve(head,sum);
+            
+        }else{
+            mp[sum]++;
+        }
+        curr=curr->next;
+    }
+    return head;
+
+    }
+};
