7e1feb57140c8f3624560dc230d30f0139b31b8f
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2024-01-02 15:36:48 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (13.04%) - LeetSync

--- 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.cpp
+++ 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.cpp
@@ -1,17 +1,46 @@
 class Solution {
-
-    int solve(unsigned long d,unsigned long need){
-       return need * d / (d-1) - (need % (d-1) ? 0 : 1);
-    }
-   
-
 public:
     int minimizeSet(int d1, int d2, int u1, int u2) {
-         long hcf=__gcd(d1,d2);
-            long l= d1/hcf*d2;
-        int s=u1+u2;
-
-        return max({solve(l,s),solve(d1,u1),solve(d2,u2)});
         
+        long hcf=__gcd(d1,d2);
+        long l=1,r=INT_MAX,lcm=(d1/hcf*d2);
+        // cout<<l<<" ";
+        while(l<=r){
+            int mid = l + (r - l) / 2;
+            int a = mid / d1, b = mid / d2;
+            if (u1 <= mid - a && u2 <= mid - b && u1 + u2 <= (mid - (mid / lcm))) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 };
+
+// #define ll long long
+// class Solution {
+// private:
+//     int gcd(int a, int b) {
+//         if (!b) return a;
+//         return gcd(b, a % b);
+//     }
+//     int lcm(ll a, ll b) {
+//         ll temp = (a / gcd(a, b)) * b;
+//         return temp > INT_MAX ? INT_MAX : temp;
+//     }
+// public:
+//     int minimizeSet(int divisor1, int divisor2, int uc1, int uc2) {
+//         int l = 1, r = INT_MAX, LCM = lcm(divisor1, divisor2);
+//         while (l <= r) {
+            // int mid = l + (r - l) / 2;
+            // int a = mid / divisor1, b = mid / divisor2;
+            // if (uc1 <= mid - a && uc2 <= mid - b && uc1 + uc2 <= (mid - (mid / LCM))) {
+            //     r = mid - 1;
+            // } else {
+            //     l = mid + 1;
+            // }
+//         }
+//         return l;
+//     }
+// };
