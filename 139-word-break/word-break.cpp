35c01c978be8806a4be931bc24e432b1162b2344
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2024-01-06 23:53:51 +0530

    Time: 43 ms (8.02%) | Memory: 20.2 MB (7.48%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+    bool solve(int i,int n,string s,map<string,int>&mp,vector<int>&dp){
+        if(i>=n){
+            return 1;
+        }
+
+        if(mp.count(s.substr(i,n-i))){
+            return 1;
+        }
+       
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        for(int j=0;j<n;j++){
+            string temp=s.substr(i,j+1);
+            if(mp.count(temp)){
+                if(solve(i+j+1,n,s,mp,dp))
+                return dp[i]=1;
+            }
+        }
+        return dp[i]=0;
+    }
+
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        map<string,int>mp;
+        int n=s.size();
+        for(string a:wordDict){
+            mp[a]++;
+        }
+        string temp;
+        vector<int>dp(n,-1);
+        return solve(0,n,s,mp,dp);
+    }
+};
