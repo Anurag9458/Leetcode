4bb79a405d2ce3a6f9cb7a83cff5448a0bc6138b
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2024-01-23 15:34:30 +0530

    Time: 87 ms (25.41%) | Memory: 8.4 MB (15.45%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,25 +1,44 @@
 class Solution {
 
-int solve(int e,int f,vector<vector<int>>&dp){
-    if(f==0||f==1){
-        return f;
-    }
-    if(e==1){
-        return f;
+// int solve(int e,int f,vector<vector<int>>&dp){
+//     if(f==0||f==1){
+//         return f;
+//     }
+//     if(e==1){
+//         return f;
+//     }
+
+//     if(dp[e][f]!=-1){
+//         return dp[e][f];
+//     }
+
+//     int mini=1e9;
+
+//     for(int i=1;i<=f;i++){
+//         int temp=1+max(solve(e-1,i-1,dp),solve(e,f-i,dp));
+//         mini=min(temp,mini);
+//     }
+//     return dp[e][f]=mini;
+// }
+
+  int solve(int k,int n,vector<vector<int>>&dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(k==1){
+            return n;
+        }
+        if(dp[k][n]!=-1){
+            return dp[k][n];
+        }
+        int mini=1e9;
+        for(int i=1;i<=n;i++){
+            int temp=1+max(solve(k-1,i-1,dp),solve(k,n-i,dp));
+            mini=min(mini,temp);
+        }
+        return dp[k][n]=mini;
     }
 
-    if(dp[e][f]!=-1){
-        return dp[e][f];
-    }
-
-    int mini=1e9;
-
-    for(int i=1;i<=f;i++){
-        int temp=1+max(solve(e-1,i-1,dp),solve(e,f-i,dp));
-        mini=min(temp,mini);
-    }
-    return dp[e][f]=mini;
-}
 
 public:
     int twoEggDrop(int n) {
