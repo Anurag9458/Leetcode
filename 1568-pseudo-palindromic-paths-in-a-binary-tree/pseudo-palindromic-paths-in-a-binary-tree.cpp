6ddb52ab5ab2d6305f2a1fa045161f4709c32854
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2024-01-24 07:19:00 +0530

    Time: 1958 ms (5.09%) | Memory: 791.9 MB (5.09%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+    bool check(unordered_map<int,int>&mp){
+        // map<int,int>mp;
+        // for(int i:arr){
+        //     mp[i]++;
+        // }
+        int  count=0;
+        for(auto it:mp){
+            if(it.second%2){
+                count++;
+            }
+        }
+
+        return count<=1;
+    }
+
+    int ans=0;
+
+    void solve(TreeNode* root,unordered_map<int,int>temp){
+        if(!root){
+            return;
+        }
+
+        if(!root->left && !root->right){
+            temp[root->val]++;
+            if(check(temp)){
+                ans++;
+            }
+            return;
+        }
+
+        temp[root->val]++;
+        solve(root->left,temp);
+        solve(root->right,temp);
+
+    }
+
+
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+        // vector<int>temp;
+        unordered_map<int,int>temp;
+        if(!root->left && !root->right){
+            return 1;
+        }
+        solve(root,temp);
+
+        return ans;
+    }
+};
