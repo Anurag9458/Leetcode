539653f3ef0af2462f2c76df8d03c69fb4e41e05
Anurag9458 <95023008+Anurag9458@users.noreply.github.com>
2024-02-28 14:27:21 +0530

    Time: 7 ms (79.35%) | Memory: 20 MB (88.43%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -10,25 +10,40 @@
  * };
  */
 class Solution {
+    int ans,maxi=-1;
+
+    void solve(TreeNode *root,int d){
+        if(!root)return;
+        if(d>maxi){
+            maxi=d;
+            ans=root->val;
+        }
+        solve(root->left,d+1);
+        solve(root->right,d+1);
+    }
+
 public:
     int findBottomLeftValue(TreeNode* root) {
-        int ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            ans=q.front()->val;
-            while(n--){
-                auto it=q.front();
-                q.pop();
-                if(it->left){
-                    q.push(it->left);
-                }
-                if(it->right){
-                    q.push(it->right);
-                }
-            }
-        }
+        // int ans;
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int n=q.size();
+        //     ans=q.front()->val;
+        //     while(n--){
+        //         auto it=q.front();
+        //         q.pop();
+        //         if(it->left){
+        //             q.push(it->left);
+        //         }
+        //         if(it->right){
+        //             q.push(it->right);
+        //         }
+        //     }
+        // }
+        
+        solve(root,0);
+
         return ans;
     }
 };
